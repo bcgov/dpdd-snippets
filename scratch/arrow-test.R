04552a0655781e922123343bfedc07f39801dad3
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2020-02-03 17:00:47 -0800

    more trying

--- scratch/arrow-test.R
+++ scratch/arrow-test.R
@@ -10,12 +10,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
 ## get arrow v0.16 ------------------------------------------------------------
-# options(repos = c(
-#   "https://dl.bintray.com/ursalabs/arrow-r",
-#   getOption("repos")
-#   ))
-# install.packages("arrow")
+# install.packages("arrow", repos = "https://dl.bintray.com/ursalabs/arrow-r")
+
 #should be on CRAN in days?
 
 
@@ -25,10 +23,12 @@ library(arrow)
 library(tictoc)
 
 
+## fake data -----------------------------------------------------------------
+
 ##  make some fake data
-# create_fake_data <- function(number_of_rows) {
+create_fake_data <- function(number_of_rows) {
 # fd <-   data.frame(sample(1:100, number_of_rows, replace = TRUE),
-#                        sample(seq(as.Date('1990/01/01'), as.Date('2010/01/01'),
+#                        sample(seq(as.Date('1990-01-01'), as.Date('2010-01-01'),
 #                                   by = "day"), number_of_rows, replace = TRUE),
 #                        sample(c("male", "female", "unknown"), number_of_rows,
 #                               replace = TRUE),
@@ -40,10 +40,8 @@ library(tictoc)
 # }
 #
 # fd <- create_fake_data(1E8)
-
+#
 # write_parquet(fd, "data/one_hundred_million_rows3.parquet")
-
-
 
 
 
@@ -53,17 +51,25 @@ ds <- open_dataset("data")
 
 tic()
 ds %>%
-  filter(gender != "unknown",
+  dplyr::filter(gender != "unknown",
          # date > as.Date("2000-01-01"), #crashing rstudio
          some_letter == "c") %>%
-  select(gender, some_number, some_letter) %>%
+  select(gender, some_number) %>%
   group_by(gender) %>%
   collect() %>%
   summarise(mean_number = mean(some_number),
             sum_number = sum(some_number))
-
 toc()
 
 
 
+## reprex  ------------------------------------------------------------
+
+read_parquet(system.file("v0.7.1.parquet", package="arrow")) %>%
+  dplyr::mutate(date = sample(seq(as.Date('1990-01-01'), as.Date('2010-01-01'),
+                                   by = "day"), 10, replace = TRUE)) %>%
+  write_parquet("tmp/parquet_with_date_col.parquet")
+
+read_parquet("tmp/parquet_with_date_col.parquet") %>%
+  dplyr::filter(date > as.Date("2000-01-01"))
 
